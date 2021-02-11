58b2988e71cfb631e9de4c976c7dfe8ffbf38145
Ramona Zmole <ramona.zmole@evozon.com>
2021-02-11 19:13:33 +0200

    try selenium 4

--- Selenium4/Selenium4/UnitTest1.cs
+++ Selenium4/Selenium4/UnitTest1.cs
@@ -1,25 +1,45 @@
+using System.Threading;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
 using OpenQA.Selenium;
 using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.DevTools.V87.Network;
 
 namespace Selenium4
 {
     [TestClass]
     public class UnitTest1
     {
-        public static IWebDriver Driver;
+        public static ChromeDriver Driver;
 
         [TestInitialize]
         public void BeforeAll()
         {
             Driver = new ChromeDriver();
-            Driver.Navigate().GoToUrl("https://www.google.com/");
+            Driver.Navigate().GoToUrl("http://automationpractice.com/index.php");
         }
 
         [TestMethod]
         public void TestMethod1()
         {
-            Assert.AreEqual("Google", Driver.Title);
+            Assert.AreEqual("My Store", Driver.Title);
+
+            var searchInput = Driver.FindElement(By.Name("search_query"));
+            var searchButton = Driver.FindElement(By.Name("submit_search"));
+            searchInput.SendKeys("demo");
+            searchButton.Click();
+
+            searchInput = Driver.FindElement(By.Name("search_query"));
+            searchInput.SendKeys(" demo 2");
+
+            Driver.FindElement(RelativeBy.WithTagName("button").Above(By.Id("block_top_menu"))).Click();
+            searchInput = Driver.FindElement(By.Name("search_query"));
+            searchInput.SendKeys(" xxx");
+
+            var session = Driver.CreateDevToolsSession();
+
+            var blockedUrlSettings = new SetBlockedURLsCommandSettings();
+            blockedUrlSettings.Urls = new string[] { "http://demos.bellatrix.solutions/wp-content/uploads/2018/04/440px-Launch_Vehicle__Verticalization__Proton-M-324x324.jpg" };
+          //  session..SetBlockedURLs(blockedUrlSettings);
         }
 
         [TestCleanup]
